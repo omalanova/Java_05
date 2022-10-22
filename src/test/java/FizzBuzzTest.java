f4826e34f0c72cfeb370fb90c59bad718fc30e31
omalanova <oyuna_eles@mail.ru>
2022-10-22 23:32:49 +0800

    FizzBuzztest

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,25 @@
+import org.testng.annotations.Test;
+
 public class FizzBuzzTest {
+
+    // 1. Positive testing Happy path
+    // if (start <= end)
+    // return array;
+    @Test
+
+    public void testStartLessThanEnd_HappyPath() {
+        //AAA
+        //arrange
+        int start = 1;
+        int end = 20;
+        String[] expectedResult
+    }
+
+
+
+            // start == end
+
+    //2. Positive testing
+    // if (start <= end)
+    //return new String[0];
 }
